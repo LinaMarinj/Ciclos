cb6e3273b1077a9731ee186e12db9d78f2fa5674
lina marin <linamarin2011@gmail.com>
2024-08-27 08:55:11 -0500

    se agregan los cilos for y while

---
+++ script.js
@@ -0,0 +1,37 @@
+/* for (let index = 0; index < 5; index++) {
+    let numeroUno = Number(prompt("Ingrese un número: "));
+    let numeroDos = Number(prompt("Ingrese otro número: "));
+    let operacion = prompt("Seleccione la opción: \n1 - Sumar \n2 - Restar \n3 - Multiplicar \n4 - Dividir ");
+
+
+    switch (operacion) {
+        case "1":
+            console.log("La suma es: " + (numeroUno + numeroDos));
+            break;
+
+
+        case "2":
+            console.log("La resta es: " + (numeroUno - numeroDos));
+
+            break;
+
+        case "3":
+            console.log("La multiplicación es: " + (numeroUno * numeroDos));
+            break;
+
+        case "4":
+            console.log("La división es: " + (numeroUno / numeroDos));
+            break;
+
+    }
+
+} */
+
+
+
+let index = 10;
+
+while (index < 20) {
+    console.log("Ciclo mientras");
+    index++;
+}
